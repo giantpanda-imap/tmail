f3738988b6c5f683fb606561288f85cb3d481c2b
TBK <tbk@jjtc.eu>
2018-11-08 21:10:45 +0100

    Correct servicename to pop3

--- src/ipopd/ipop3d.c
+++ src/ipopd/ipop3d.c
@@ -107,7 +107,7 @@ int main (int argc,char *argv[])
     (((s = strrchr (argv[0],'/')) || (s = strrchr (argv[0],'\\'))) ?
      s+1 : argv[0]) : "ipop3d";
 				/* set service name before linkage */
-  mail_parameters (NIL,SET_SERVICENAME,(void *) "pop");
+  mail_parameters (NIL,SET_SERVICENAME,(void *) "pop3");
 #include "linkage.c"
 				/* initialize server */
   server_init (pgmname,"pop3","pop3s",clkint,kodint,hupint,trmint,NIL);
