f09ae85797d2be5cce93373f7976d4670015b563
TBK <tbk@jjtc.eu>
2018-11-12 16:59:04 +0100

    restrictBox=-1 by default

--- src/c-client/unix/env_unix.c
+++ src/c-client/unix/env_unix.c
@@ -73,7 +73,7 @@ static char *sslDHParams = NIL; /* specified SSL Diffie-Hellman parameters file,
 static short anonymous = NIL;   /* is anonymous */
 static short blackBox = NIL;    /* is a black box */
 static short closedBox = NIL;   /* is a closed box (uses chroot() jail) */
-static long restrictBox = NIL;  /* is a restricted box */
+static long restrictBox = -1;   /* is a restricted box */
 static short has_no_life = NIL; /* is a cretin with no life */
                                 /* block environment init */
 static short block_env_init = NIL;
